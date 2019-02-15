688eb8c618cbb31577a156d3705fd6128f80ff32
Kristen Hallstrom <klhalls89@gmail.com>
2019-02-14 20:32:53 -0700

    Start dropdown filter

--- src/components/GemsPage.js
+++ src/components/GemsPage.js
@@ -19,18 +19,32 @@ class GemsPage extends Component {
     })
   }
 
-  gemsByColor = () => {
-    let filteredGems = this.props.gems.filter(gem => {
+  gemsByName = () => {
+    let searchedGems = this.props.gems.filter(gem => {
       return gem.name.toLowerCase().includes(this.state.searchInput);
       })
     this.setState({
-      filteredGems
+      searchedGems
     })
+  }
+
+  selectColor = (e) => {
+    let selectedGems = [];
+    let clickedColor = e.target.value
+    selectedGems = this.props.gems.filter(gem => {
+      return gem.color.includes(clickedColor)
+    })
+
+    //e.target.value 
+    //iterate over the gems array
+    //second iterator for each color
+    //if the the target value is included in the array
+    //put that item into the select gems array
+
   }
 
   render() {
     const GemPageDisplay = this.state.GemPage ? { display: 'none' } : {};
-
     const gemCards = this.state.filteredGems.map(gem => {
       return <GemCard key={gem.id} gemName={gem.name} gemFam={gem.family} gemImg={gem.image}/>
     })
@@ -41,19 +55,19 @@ class GemsPage extends Component {
           <h1>Evermore Gems</h1>
           <h2>Instructions</h2>
           <input value={this.state.searchInput} onChange={this.updateInputValue} type="text" />
-          <button onClick={this.gemsByColor}>Search</button>
-          <select>
-            <option value="white"> White </option>
-            <option value="red"> Red </option>
-            <option value="orange"> Orange </option>
-            <option value="yellow"> Yellow </option>
-            <option value="green"> Green </option>
-            <option value="blue"> Blue </option>
-            <option value="purple"> Purple </option>
-            <option value="pink"> Pink </option>
-            <option value="brown"> Brown </option>
-            <option value="grey"> Grey </option>
-            <option value="black"> Black </option>
+          <button onClick={this.gemsByName}>Search</button>
+          <select onChange={this.selectColor}>
+            <option value="White">White</option>
+            <option value="Red">Red</option>
+            <option value="Orange">Orange</option>
+            <option value="Yellow">Yellow</option>
+            <option value="Green">Green</option>
+            <option value="Blue">Blue</option>
+            <option value="Purple">Purple</option>
+            <option value="Pink">Pink</option>
+            <option value="Brown">Brown</option>
+            <option value="Grey">Grey</option>
+            <option value="Black">Black</option>
           </select>
         </div>
         <div className="result">
